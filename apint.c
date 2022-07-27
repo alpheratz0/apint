30e597ed187a5b832fe331bf16abca3f48fc7d6e
alpheratz0 <alpheratz99@protonmail.com>
2022-07-27 20:50:38 -0300

    add cowthink message

--- apint.c
+++ apint.c
@@ -13,6 +13,17 @@
 	this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 	Place, Suite 330, Boston, MA 02111-1307 USA
 
+	 ________
+	( tomcat )
+	 --------
+	   o
+	    o
+
+	     |\_/|
+	     |o o|__
+	     --*--__\
+	     C_C_(___)
+
 */
 
 #include <stdio.h>
