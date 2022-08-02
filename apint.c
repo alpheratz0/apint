0bc013a144ebf41440d9a2c0be0aec3c8e65ffea
alpheratz0 <alpheratz99@protonmail.com>
2022-08-02 13:43:39 -0300

    clean `usage` function

--- apint.c
+++ apint.c
@@ -404,21 +404,10 @@ match_opt(const char *in, const char *sh, const char *lo)
 	return (strcmp(in, sh) == 0) || (strcmp(in, lo) == 0);
 }
 
-static inline void
-print_opt(const char *sh, const char *lo, const char *desc)
-{
-	printf("%7s | %-25s %s\n", sh, lo, desc);
-}
-
 static void
 usage(void)
 {
-	puts("Usage: apint [ -fhv ] [ -l FILE ]");
-	puts("Options are:");
-	print_opt("-f", "--fullscreen", "start in fullscreen mode");
-	print_opt("-h", "--help", "display this message and exit");
-	print_opt("-v", "--version", "display the program version");
-	print_opt("-l", "--load", "load ppm image");
+	puts("usage: apint [-fhv] [-l FILE]");
 	exit(0);
 }
 
