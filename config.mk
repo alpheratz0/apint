68d8eb0ab026c40f091af34964dcff2296876127
alpheratz0 <alpheratz99@protonmail.com>
2022-09-18 20:08:53 -0300

    bump version to 0.2.0

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # Copyright (C) 2022 <alpheratz99@protonmail.com>
 # This program is free software.
 
-VERSION   = 0.1.0
+VERSION   = 0.2.0
 
 CC        = cc
 CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os -DVERSION=\"$(VERSION)\"
