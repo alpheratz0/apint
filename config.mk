cb5a4fd269ded39ee16353e3b8cb207538cbd52b
alpheratz0 <alpheratz99@protonmail.com>
2022-12-29 18:07:18 -0300

    link against xcb-shm

--- config.mk
+++ config.mk
@@ -6,7 +6,7 @@ VERSION   = 0.4.0
 CC        = cc
 INCS      = -I/usr/X11R6/include
 CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
-LDLIBS    = -lxcb -lxcb-image -lxcb-cursor -lxcb-keysyms -lxcb-icccm -lxcb-xkb -lpng -lm -lsaveas
+LDLIBS    = -lxcb -lxcb-image -lxcb-cursor -lxcb-keysyms -lxcb-icccm -lxcb-xkb -lpng -lm -lsaveas -lxcb-shm
 LDFLAGS   = -L/usr/X11R6/lib -s
 
 PREFIX    = /usr/local
