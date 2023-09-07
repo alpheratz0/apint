81234c6be6446208afd2333211ff1e5ddecf655a
alpheratz0 <alpheratz99@protonmail.com>
2023-09-07 09:06:23 -0300

    bump version to 0.8.1

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # Copyright (C) 2022-2023 <alpheratz99@protonmail.com>
 # This program is free software.
 
-VERSION=0.8.0
+VERSION=0.8.1
 CC=cc
 INCS=-I/usr/X11R6/include -Iinclude
 CFLAGS=-std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
